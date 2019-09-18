9b3e7124890a2ab0ad915e99f9e70a02669d5607
jkim0715 <gjr191@naver.com>
2019-09-18 10:35:09 +0900

    suspend

--- day02/src/day02/Suspend.java
+++ day02/src/day02/Suspend.java
@@ -1,10 +1,45 @@
 package day02;
 
+import java.util.Scanner;
+
+class Sus implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("*");
+//			System.out.println(Thread.currentThread().getName()+" "+Thread.currentThread().getId());
+		}
+		
+	}
+	
+
+
 public class Suspend {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Thread t1 = null;
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int cmd = sc.nextInt();
+			if(cmd ==1 ) {
+				t1 = new Thread(new Sus(),"s1");
+				t1.start();
+			}else if(cmd == 2) {
+				//unable to suspend
+				t1.suspend();
+			}else if(cmd == 3) {
+				t1.resume();
+			}else if(cmd == 4) {
+				//stoped point is not clear... deprecated...
+				t1.stop();
+			}else if(cmd ==9) {
+				return;
+			}
+			
+		}
+		
+		
 	}
 
 }
