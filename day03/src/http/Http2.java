7c7581e0688fb875765dd106024c4510cbf07489
jkim0715 <gjr191@naver.com>
2019-09-20 09:43:38 +0900

    Download a file from server

---
+++ day03/src/http/Http2.java
@@ -0,0 +1,36 @@
+package http;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Http2 {
+	//Downloading File from server
+	public static void main(String[] args) throws Exception {
+		//1. Make URL
+		String urlstr = "http://70.12.60.90/test/oracle.zip";
+		URL url = new URL(urlstr);
+		
+		//2. Prepare InputStream
+		InputStream is = url.openStream();
+		BufferedInputStream bis = new BufferedInputStream(is);
+		
+		//2-1. Make FileOutputStream 
+		String filename = "oracle.zip";
+		FileOutputStream fos = new FileOutputStream(filename);
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+				
+		//3. Download File
+		int data = 0;
+		while((data = bis.read())!= -1) {
+			bos.write(data);
+		}	
+		System.out.println("Finish");
+		is.close();
+		bos.flush();
+		bos.close();
+	}
+
+}
